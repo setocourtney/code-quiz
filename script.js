a0d3f8344a5e4433dd0128343b12d51f9a823829
Courtney Seto <courtneyjseto@gmail.com>
2020-01-06 11:44:42 -0800

    outlined pseudo code for script.js and view-scores.js, added highscores.html

---
+++ script.js
@@ -0,0 +1,25 @@
+
+//start button onclick event
+//initialize timer
+//initialize score
+//set interval to # questions * 15 seconds
+
+//create array of titles from questions
+//step through array of titles
+//store array of choices from questions using title key
+//store answer variable
+//loop through array of choices to create buttons for each
+//add click even to buttons
+//if text of "this" is equal to the answer, move on to next title in array
+//if text of "this" does not equipal answer, deduct time from timer
+//if looped through the array, end game store score
+//if timer runs out, end game and store score
+
+//save score and initials as object
+//use JSON to stringify and save to object in local storage
+
+//highscores script
+
+//pull object from local storage
+//use JSON to parse scores
+//loop through scores and create list items
